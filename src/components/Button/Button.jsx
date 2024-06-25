e70fdc5c62a4a1de48f99c262cdc25e9542029be
PereiraGus <gustavo.oito@hotmail.com>
2024-06-25 19:02:20 -0300

    275 Ajustes de fidelidade de protótipo no botão.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import styles from "./button.module.css"
 export default function Button({insideText,onClick}){
     return (
         <>
-            <button onClick={onClick} className={styles.customButton}>{insideText}</button>
+            <button onClick={onClick}>{insideText}</button>
         </>
     )
 }
