175969defe2b0d7ae42797af0912c8b107f84b9c
Julia Justino <justinostephany490@gmail.com>
2024-07-23 21:46:08 -0300

    as

--- src/pages/text.jsx
+++ src/pages/text.jsx
@@ -5,16 +5,19 @@ import Input from "../components/itemMenu/itemMenu.jsx";
 import T from "../components/Toast/toastSucesso.jsx"
 import TI from "../components/Toast/toastInvalido.jsx"
 import ERRO from "../components/Toast/toastErro.jsx"
+import Menu from "../components/MenuSuperior/MenuSuperior.jsx"
 
 
 const ItemDoMenu = () => {
     const [inputValue, setInputValue] = useState('');
     return (
         <div>
-            <Input icon={"question-circle"} title="Usuário" />
+            <Menu icon={"bars"} title="PÁGINA INICIAL" />
+            {/* <Input icon={"question-circle"} title="Usuário" />
             <T icon={"circle-check"} title="Entrada registrada com sucesso." />
             <TI icon={"triangle-exclamation"} title="Este item não possui quantidade no estoque para ser retirada." />
-            <ERRO icon={"triangle-exclamation"} title="Defina um tipo e quantidade para o ajuste do produto selecionado." />
+            <ERRO icon={"triangle-exclamation"} title="Defina um tipo e quantidade para o ajuste do produto selecionado." /> */}
+            
         </div>
     );
 }
