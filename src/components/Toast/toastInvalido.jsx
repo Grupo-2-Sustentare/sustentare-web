088bac9a7a1e3fe1ca788d9d2c1b03a3a60ca665
Julia Justino <justinostephany490@gmail.com>
2024-07-23 21:10:56 -0300

    tipos de toast

---
+++ src/components/Toast/toastInvalido.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './toast.module.css';
+
+export default function Toast ({icon, title}){
+  return (
+    <div className={styles.ToastInvalido}>
+      <FontAwesomeIcon icon={icon} className={styles.icone}/>
+      <span className={styles.title}>{title}</span>
+    </div>
+  );
+}
--- src/pages/text.jsx
+++ src/pages/text.jsx
@@ -3,6 +3,9 @@ import React, { useState } from "react"; // Importa React e o hook useState para
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Input from "../components/itemMenu/itemMenu.jsx";
 import T from "../components/Toast/toastSucesso.jsx"
+import TI from "../components/Toast/toastInvalido.jsx"
+import ERRO from "../components/Toast/toastErro.jsx"
+
 
 const ItemDoMenu = () => {
     const [inputValue, setInputValue] = useState('');
@@ -10,6 +13,8 @@ const ItemDoMenu = () => {
         <div>
             <Input icon={"question-circle"} title="Usuário" />
             <T icon={"circle-check"} title="Entrada registrada com sucesso." />
+            <TI icon={"triangle-exclamation"} title="Este item não possui quantidade no estoque para ser retirada." />
+            <ERRO icon={"triangle-exclamation"} title="Defina um tipo e quantidade para o ajuste do produto selecionado." />
         </div>
     );
 }
