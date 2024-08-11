c3ca898347a465e32f62c8ff23e9b572597f2e6c
Julia Justino <justinostephany490@gmail.com>
2024-08-10 23:12:18 -0300

    item com imagem

--- src/pages/text.jsx
+++ src/pages/text.jsx
@@ -1,24 +1,23 @@
-// Importa toast para exibir mensagens de sucesso ou erro
-import React, { useState } from "react"; // Importa React e o hook useState para gerenciamento de estado
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Input from "../components/itemMenu/itemMenu.jsx";
-import T from "../components/Toast/toastSucesso.jsx"
-import TI from "../components/Toast/toastInvalido.jsx"
-import ERRO from "../components/Toast/toastErro.jsx"
-import Menu from "../components/MenuSuperior/MenuSuperior.jsx"
-
+import T from "../components/Toast/toastSucesso.jsx";
+import TI from "../components/Toast/toastInvalido.jsx";
+import ERRO from "../components/Toast/toastErro.jsx";
+import Img from "../components/itemListaImg/itemListaImg.jsx";
 
 const ItemDoMenu = () => {
     const [inputValue, setInputValue] = useState('');
+
     return (
         <div>
-            <Menu icon={"bars"} title="PÁGINA INICIAL" />
-            {/* <Input icon={"question-circle"} title="Usuário" />
+            <Input icon={"house"} title="Página inicial" />
             <T icon={"circle-check"} title="Entrada registrada com sucesso." />
-            <TI icon={"triangle-exclamation"} title="Este item não possui quantidade no estoque para ser retirada." />
-            <ERRO icon={"triangle-exclamation"} title="Defina um tipo e quantidade para o ajuste do produto selecionado." /> */}
-            
+            {/* <TI icon={"triangle-exclamation"} title="Este item não possui quantidade no estoque para ser retirada." /> */}
+            {/* <ERRO icon={"triangle-exclamation"} title="Defina um tipo e quantidade para o ajuste do produto selecionado." /> */}
+            <Img  title="Antônio" desc="Usuário(a)" adressImg="https://i0.wp.com/ochin.com.br/wp-content/uploads/2023/04/1.jpg?fit=1024%2C974&ssl=1"/>	
         </div>
     );
 }
+
 export default ItemDoMenu;
