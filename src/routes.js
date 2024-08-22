9e8bd25b0b9b48a1d5d6adab4dd71edfc6ab575d
GabrielMichelon2004 <gabriel.michelon@sptech.school>
2024-08-22 18:54:08 -0300

    Componente RedirectionList 100%

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Cadastro from "./pages/Cadastro/Cadastro";
-import ItemPage from './pages/item/ItemPage';
 
 import Debug from "./pages/Debug/Debug";
 function Rotas() {
@@ -13,7 +12,6 @@ function Rotas() {
                     <Route path="/" element={<Login />} />
                     <Route path={"/login"} element={<Login />} />
                     <Route path={"/cadastro"} element={<Cadastro />} />
-                    <Route path="/item/:item" element={<ItemPage />} />
                     <Route path={"/debug"} element={<Debug />} />
                 </Routes>
             </BrowserRouter>
