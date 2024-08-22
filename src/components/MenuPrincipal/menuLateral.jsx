2b95deb8f68b290d1a8cf0df490630a98aa4faa9
Alexandra Harumi Yara <alexandra.yara@sptech.school>
2024-08-22 18:23:39 -0300

    Barra Lateral sem transição - ajustar botão Sair e adicionar transição

--- src/components/MenuPrincipal/menuLateral.jsx
+++ src/components/MenuPrincipal/menuLateral.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import Input from "../itemMenu/itemMenu.jsx";
+import ItemMenu from "../MenuItem/MenuItem.jsx"
 import style from './menuLateral.module.css';
 
 const MenuLateral = ({ userName, userImage }) => {
     return (
-        <div className={style.sidebar}>
-            <div className={style.sidebarHeader}>
-                <h1>Paralelo 19</h1>
-            </div>
-            <div className={style.sidebarMenu}>
-            <Input icon={"house"} title="Página inicial" />
-                {/* Aqui vai o seu componente de itens do menu */}
-
-            </div>
-            <div className={style.sidebarFooter}>
-            {/* alt={`${userName}`} */}
-                <img src={userImage} className={style.userImage} />
-                <div className={style.userInfo}>
-                    <span className={style.userName}>{userName.toUpperCase()}</span>
-                    <span className={style.logoutButton}>Sair</span>
+        <div className={style.divSideBar}>
+            <div className={style.sidebar}>
+                <div className={style.sidebarHeader}>
+                    <h1>Paralelo 19</h1>
                 </div>
+                    <div className={style.sidebarMenu}>
+                <ItemMenu icon={"house"} title="Página inicial" />
+                 {/* Aqui vai o seu componente de itens do menu */}
+
+                </div>
+                <div className={style.sidebarFooter}>
+                {/* alt={`${userName}`} */}
+                    <img src={userImage} className={style.userImage} />
+                    <div className={style.userInfo}>
+                        <span className={style.userName}>{userName.toUpperCase()}</span>
+                        <span className={style.logoutButton}>Sair</span>
+                    </div>
+                </div>
+            </div>
+            <div className={style.sideSideBar}>   
             </div>
         </div>
     );
