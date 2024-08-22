3ed5e118eb647e091dcb0651872629dc7dfd184a
PereiraGus <gustavo.oito@hotmail.com>
2024-08-22 18:15:05 -0300

    Criação do IconTextInput.

---
+++ src/components/IconTextInput/IconTextInput.jsx
@@ -0,0 +1,26 @@
+import styles from "./iconTextInput.module.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+const TIPOS_PERMITIDOS = ["text", "password"]
+
+export default function IconTextInput({ label, value, type="text", onChange}){
+
+    // Se o tipo não for permitido, dá exception.
+    if (!TIPOS_PERMITIDOS.includes(type)) throw new Error("Tipo não permitido ou não implementado. Medida de qualidade.")
+
+    // Criando a label, caso tenha sido informada.
+    let tagLabel = null;
+    if (label !== undefined){
+        tagLabel = <label>{label}</label>
+    }
+
+    return (
+        <div className={styles.textInput}>
+            {tagLabel}
+            <div>
+                <input value={value} type={type} onChange={onChange}/>
+                <FontAwesomeIcon className={styles.icon} icon={"magnifying-glass"}></FontAwesomeIcon>
+            </div>
+        </div>
+    )
+}
+                    
