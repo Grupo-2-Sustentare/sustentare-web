e79b889e0e63de03ef5e13f6ece6e61de61f44bc
PereiraGus <gustavo.oito@hotmail.com>
2024-08-22 19:58:56 -0300

    Correção do debug via .env.

--- src/routes.js
+++ src/routes.js
@@ -5,17 +5,28 @@ import Cadastro from "./pages/Cadastro/Cadastro";
 import Debug from "./pages/Debug/Debug";
 
 function Rotas() {
-    return (
-        <>
+    if (process.env.REACT_APP_MODO_DEBUG === "1"){
+        return (<>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path={"/login"} element={<Login />} />
-                    <Route path={"/cadastro"} element={<Cadastro />} />
-                    <Route path={"/debug"} element={<Debug />} />
+                    <Route path="/" element={<Debug />} />
                 </Routes>
-            </BrowserRouter>
-        </>
-    )
+           </BrowserRouter>
+        </>)
+    }
+    else{
+        return (
+            <>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path={"/login"} element={<Login />} />
+                        <Route path={"/cadastro"} element={<Cadastro />} />
+                        <Route path={"/debug"} element={<Debug />} />
+                    </Routes>
+                </BrowserRouter>
+            </>
+        )
+    }
 }
 export default Rotas;
