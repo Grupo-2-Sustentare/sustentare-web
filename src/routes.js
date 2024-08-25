21e086e031967249ac6224d1e55fde278397a241
Alexandra Harumi Yara <alexandra.yara@sptech.school>
2024-08-25 18:40:47 -0300

    Analisar

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Debug from "./pages/debug/Debug";
 import Login from "./pages/login-e-entrada/Login/Login";
-import Cadastro from "./pages/Cadastro/Cadastro";
-import Debug from "./pages/Debug/Debug";
+import MainMenu from "./pages/login-e-entrada/MainMenu/MainMenu";
+import NewMovement from "./pages/cadastros-de-estoque/NewMovement/NewMovement"
+import ConfigurationMenu from "./pages/configuracoes-de-estoque/ConfigurationMenu/ConfigurationMenu";
+import Cadastro from "./pages/configuracoes-de-acesso/Cadastro";
 import ExibirComponentes from "./pages/teste";
+
 function Rotas() {
     if (process.env.REACT_APP_MODO_DEBUG === "1"){
         return <BrowserRouter><Routes><Route path="/" element={<Debug />} /></Routes></BrowserRouter>
