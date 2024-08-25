953891ca843aace546c104fc55a32fb99328f3bc
Alexandra Harumi Yara <alexandra.yara@sptech.school>
2024-08-25 16:42:36 -0300

    Menu Lateral funcionando com menu superior, mas sem transição suave

--- src/components/MenuSuperior/menuSuperior.jsx
+++ src/components/MenuSuperior/menuSuperior.jsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './menuSuperior.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import MenuLateral from '../MenuLateral/menuLateral';
 
-export default function MenuSuperior ({title}) {
+var userImage = "https://i0.wp.com/ochin.com.br/wp-content/uploads/2023/04/1.jpg?fit=1024%2C974&ssl=1";
+var userName = "Antônio";
+
+export default function MenuSuperior({ title, showBackArrow }) {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    if (!showBackArrow) {
+      setMenuOpen(!isMenuOpen);
+    }
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  let icon;
+
+  if (showBackArrow) {
+    icon = "fa-solid fa-arrow-left";
+  } else {
+    icon = "fa-solid fa-bars";
+  }
+
   return (
-    <div className={styles.menuSuperior}>
-      <div className={styles.iconDiv}>
-      {/* <FontAwesomeIcon className={styles.icon} icon="fa-solid fa-arrow-left"/> */}
-      <FontAwesomeIcon className={styles.icon} icon="fa-solid fa-bars"/>
+    <div>
+      <div className={styles.menuSuperior}>
+        <div className={styles.iconDiv} onClick={handleMenuClick}>
+          <FontAwesomeIcon className={styles.icon} icon={icon} />
+        </div>
+        <div className={styles.titleDiv}>
+          <span className={styles.title}>{title}</span>
+        </div>
       </div>
-      <div className={styles.titleDiv}>
-      <span className={styles.title}>{title}</span>
-      </div>
+      {!showBackArrow && isMenuOpen && (
+        <div>
+          <MenuLateral class="duration-1" userName={userName} userImage={userImage} />
+          <div className={styles.overlay} onClick={closeMenu}></div>
+        </div>
+      )}
     </div>
   );
-};
+}
