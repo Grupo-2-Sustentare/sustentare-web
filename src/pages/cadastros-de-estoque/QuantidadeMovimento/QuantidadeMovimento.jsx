984ad85b7b8368310a9ec30ce3f6822912900b17
PereiraGus <gustavo.oito@hotmail.com>
2024-09-08 17:34:40 -0300

    11 - Adicionando dinamicidade para entradas e saídas.

--- src/pages/cadastros-de-estoque/QuantidadeMovimento/QuantidadeMovimento.jsx
+++ src/pages/cadastros-de-estoque/QuantidadeMovimento/QuantidadeMovimento.jsx
@@ -2,13 +2,22 @@ import styles from "./quantidadeMovimento.module.css"
 import TopBar from "../../../components/TopBar/TopBar";
 import {useNavigate} from "react-router-dom";
 import Product from "../../../components/ProductItem/Product";
-import MeasurementUnitInput from "../../../components/MeasumentTextInput/MeasurementUnitInput";
 import Switch from "../../../components/Switch/Switch";
 import Button from "../../../components/Button/Button";
+import RedirectionList from "../../../components/RedirectionList/RedirectionList";
+import {errorToast} from "../../../components/Toast/Toast";
+import MeasurementUnitInput from "../../../components/MeasumentTextInput/MeasurementUnitInput";
 
 export function QuantidadeMovimento(){
     const navigate = useNavigate()
     let p = JSON.parse(sessionStorage.getItem("productBeingEdited"))
+
+    let ehSaida = p.quantidade < 0
+    let acao = ehSaida ? "saiu do":"entrou no"
+
+    function salvarEdicao(){
+        navigate("/cadastros-de-estoque")
+    }
 
     return(<div>
         <TopBar showBackArrow={true} title={"Quantidade de movimento"}/>
@@ -17,13 +26,18 @@ export function QuantidadeMovimento(){
             <Product name={p.nome} addressImg={p.urlImagem} quantity={  `${p.quantidade} ${p.unidade}`}/>
         </div>
         <div className={styles.principal}>
-            <p>Quanto do produto entrou em estoque?</p>
-            <MeasurementUnitInput measurementUnit={p.unidade}/>
+            <p>Quanto do produto {acao} estoque?</p>
+            <MeasurementUnitInput measurementUnit={p.unidade} placeholder={0}/>
             <Switch label={"É ajuste de uma marcação anterior errada?"}/>
-            <Switch label={"Compra de última hora?"}/>
+            {ehSaida &&
+                (<RedirectionList title={"Categoria do consumo"}/>)
+            }
+            {!ehSaida &&
+                (<Switch label={"Compra de última hora?"}/>)
+            }
         </div>
         <div className={styles.containerBotao}>
-            <Button insideText={"Confirmar definições"}/>
+            <Button insideText={"Confirmar definições"} onClick={salvarEdicao}/>
         </div>
 
     </div>)
