cc8fcec4d9cd73798a6f7c33d56b692bdd3eac7e
PereiraGus <gustavo.oito@hotmail.com>
2024-09-08 17:08:37 -0300

    11 - Adição de placeholder no IconInput.

--- src/components/IconInput/IconInput.jsx
+++ src/components/IconInput/IconInput.jsx
@@ -2,7 +2,7 @@ import styles from "./iconInput.module.css"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 const TIPOS_PERMITIDOS = ["text", "password"]
 
-export default function IconInput({ label, type="text", icon="magnifying-glass", onChange}){
+export default function IconInput({ label, type="text", icon="magnifying-glass", onChange, placeholder}){
 
     // Se o tipo não for permitido, dá exception.
     if (!TIPOS_PERMITIDOS.includes(type)) throw new Error("Tipo não permitido ou não implementado. Medida de qualidade.")
@@ -17,7 +17,7 @@ export default function IconInput({ label, type="text", icon="magnifying-glass",
         <div className={styles.textInput}>
             {tagLabel}
             <div>
-                <input type={type} onChange={onChange}/>
+                <input type={type} onChange={onChange} placeholder={placeholder}/>
                 <FontAwesomeIcon className={styles.icon} icon={icon}></FontAwesomeIcon>
             </div>
         </div>
