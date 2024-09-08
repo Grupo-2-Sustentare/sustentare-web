8087eecb7bb335ebac192dfa199346d8c279bba4
PereiraGus <gustavo.oito@hotmail.com>
2024-09-07 21:23:41 -0300

    11 - Adicionando hint e opção sem label no StrechList.

--- src/components/StrechList/StrechList.jsx
+++ src/components/StrechList/StrechList.jsx
@@ -1,18 +1,18 @@
 import styles from "./StrechList.module.css";
 
 export default function StrechList({
-        titulo = "Escolha uma opção:",
-        itens=["Adicione", "seus itens", "à lista"]
+        title = "Escolha uma opção:",
+        showTitle = true,
+        hint = "Selecione...",
+        items = ["Adicione", "seus itens", "à lista"]
     }){
     return(
         <div className={styles.container}>
-            <label>{titulo}</label>
-            <select defaultValue={null}>
+            {showTitle && (<label>{title}</label>)}
+            <select defaultValue={"hint"}>
                 {/* Opção placeholder padrão e não-selecionável */}
-                <option value={null} disabled={true}>
-                    Selecione...
-                </option>
-                {itens.map(item => <option key={item} value={item}>{item}</option>)}
+                <option value={"hint"} disabled={true}>{hint}</option>
+                {items.map(item => <option key={item} value={item}>{item}</option>)}
             </select>
         </div>
     )
