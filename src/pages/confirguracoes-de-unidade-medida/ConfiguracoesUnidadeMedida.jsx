6507a6441c483cfa23a84edebea7d1a4eaa71048
GabrielMichelon2004 <gabriel.michelon@sptech.school>
2024-09-10 20:23:38 -0300

    add

--- src/pages/confirguracoes-de-unidade-medida/ConfiguracoesUnidadeMedida.jsx
+++ src/pages/confirguracoes-de-unidade-medida/ConfiguracoesUnidadeMedida.jsx
@@ -26,7 +26,7 @@ const ConfiguracoesUnidadeMedida = () => {
                 <Product addressImg={false} name="Mililitro"  quantity={["Tipo: Volume", <br />, "Abreviação: ml"]}  showCheckbox={false} icon="fa-solid fa-cube" />
                 <Product addressImg={false} name="Quilograma"    quantity={["Tipo: Massa", <br />, "Abreviação: kg"]}  showCheckbox={false} icon="fa-solid fa-hippo"/>
                 <Product addressImg={false} name="Sacas"    quantity={["Tipo: Massa", <br />, "Abreviação: kg"]}  showCheckbox={false} icon="fa-solid fa-hippo"/>
-                <Product addressImg={false} name="Quantidade Genérica"    quantity={["Tipo: Massa", <br />, "Abreviação: sacas"]}  showCheckbox={false} icon="fa-solid fa-circle"/>
+                <Product addressImg={false} name="Quantidade Genérica"    quantity={["Tipo: Massa", <br />, "Abreviação: sacas"]}  showCheckbox={false} icon="fa-solid fa-circle"/> 
             </div>
             <div className={styles.divBotao}>
             <Button insideText="Cadastrar nova unidade de medida" onClick={handleSave}/>
