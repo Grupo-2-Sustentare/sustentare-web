ca2cce277f18257776263eb8161e2b2d7f15c96c
PereiraGus <gustavo.oito@hotmail.com>
2024-09-10 18:50:54 -0300

    35 - Adição de mais customização nos ícones do Product.

--- src/components/ProductItem/Product.jsx
+++ src/components/ProductItem/Product.jsx
@@ -5,13 +5,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {alertToast} from "../Toast/Toast";
 
 export const DEFAULT_BUTTON_CONFIG = {
+    // Estilos default de cada botão. Importar em classes que deseja alterar e passar como prop para esse componente.
     "yellow": {
+        "style": {},
         "icon": "fa-solid fa-pen",
+        "iconFillInvert": false, // Quando false, o ícone será preto. Quando true, branco.
         "text": "Editar quantidade",
         "action": () => alertToast("Defina uma ação para esse botão.")
     },
     "red": {
+        "style": {},
         "icon": "fa-solid fa-trash",
+        "iconFillInvert": false,
         "text": "Remover",
         "action": () => alertToast("Defina uma ação para esse botão.")
     }
@@ -19,7 +24,7 @@ export const DEFAULT_BUTTON_CONFIG = {
 export default function Product(
     {
         addressImg, name, quantity, checkboxVariant = false, checkedByDefault = false,
-        buttonsConfig = undefined
+        buttonsConfig = undefined, fullBorderRadius=false
     }
     ) {
     // Inicializando o parâmetro que customiza os botões.
@@ -47,7 +52,7 @@ export default function Product(
     return (
       <div className={styles.product}>
         <div className={styles.mainInfo} onClick={handleClick}>
-          <img src={addressImg} alt={"Ícone do produto"}/>
+          <img src={addressImg} alt={"Ícone do produto"} className={fullBorderRadius ? styles.profileImage:""}/>
 
           <span className={styles.info}>
             <h4>{name}</h4>
@@ -63,12 +68,27 @@ export default function Product(
 
           {expanded && (
               <div className={styles.buttons}>
-                  <button className={styles.editBtn} id='botaoEdt' onClick={buttonsConfig.yellow.action}>
-                      <FontAwesomeIcon className={styles.icon} icon={buttonsConfig.yellow.icon}/>
+                  <button
+                      className={styles.editBtn} id='botaoEdt' onClick={buttonsConfig.yellow.action}
+                      style={buttonsConfig.yellow.style}
+                  >
+
+                      <FontAwesomeIcon
+                          // Aplica a classe de ícone e, se for para inverter a cor para branco, aplica essa classe
+                          // também.
+                          className={styles.icon + " " + (buttonsConfig.yellow.iconFillInvert ? styles.inverse:"")}
+                          icon={buttonsConfig.yellow.icon}
+                      />
                       {buttonsConfig.yellow.text}
                   </button>
-                  <button className={styles.removeBtn} id='botaoRemove' onClick={buttonsConfig.red.action}>
-                      <FontAwesomeIcon className={styles.icon} icon={buttonsConfig.red.icon}/>
+                  <button
+                      className={styles.removeBtn} id='botaoRemove' onClick={buttonsConfig.red.action}
+                      style={buttonsConfig.red.style}
+                  >
+                      <FontAwesomeIcon
+                          className={styles.icon + " " + (buttonsConfig.red.iconFillInvert ? styles.inverse:"")}
+                          icon={buttonsConfig.red.icon}
+                      />
                       {buttonsConfig.red.text}
                   </button>
           </div>
