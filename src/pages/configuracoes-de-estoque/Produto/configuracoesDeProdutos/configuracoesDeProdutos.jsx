55d1ce63cae5a6e3888d5ebb88a8b2387c21c3a5
GabrielMichelon2004 <gabriel.michelon@sptech.school>
2024-11-13 18:45:58 -0300

    maneira correta

--- src/pages/configuracoes-de-estoque/Produto/configuracoesDeProdutos/configuracoesDeProdutos.jsx
+++ src/pages/configuracoes-de-estoque/Produto/configuracoesDeProdutos/configuracoesDeProdutos.jsx
@@ -8,6 +8,7 @@ import IconInput from "../../../../components/IconInput/IconInput";
 import api from "../../../../api";
 import Product, { DEFAULT_BUTTON_CONFIG } from "../../../../components/ProductItem/Product";
 import { successToast } from "../../../../components/Toast/Toast";
+import axios from "axios";
 
 const ConfiguracoesProdutos = () => {
     sessionStorage.removeItem('paginaRequisicao');
@@ -22,6 +23,28 @@ const ConfiguracoesProdutos = () => {
     sessionStorage.removeItem("selectedCategoria");
     const navigate = useNavigate();
     const [produtos, setProdutos] = useState([]);
+
+    const existImagem = (idUsuario) => {
+        const awsImageUrl = `https://teste-sustentare.s3.us-east-1.amazonaws.com//itens/imagens/${idUsuario}`;
+    
+            // Faz uma requisição HEAD para verificar se a imagem existe
+            axios.get(awsImageUrl).then((response) => {
+                console.log("-----------------------------------------------------")
+                console.log(response)
+                console.log("-----------------------------------------------------")
+                // Verifica se a imagem existe (status 200)
+                if (response.ok) {
+                    return awsImageUrl;
+                } else {
+                    return "https://placehold.co/400/F5FBEF/22333B?text=Produto";
+                }
+            })
+            .catch((error) => {
+                console.error("Erro ao verificar imagem:", error);
+                return "https://placehold.co/400/F5FBEF/22333B?text=Produto";
+            });
+        }
+    
 
     useEffect(() => {
         api.get("/produtos")
@@ -43,20 +66,20 @@ const ConfiguracoesProdutos = () => {
         sessionStorage.setItem("produto_selecionado", JSON.stringify(produto)); // Salva a categoria na sessionStorage
         navigate("/editando-produto"); // Redireciona para a página de edição
     };
-    
+
     const handleRemove = (produto) => {
-        
+
         const confirmRemove = window.confirm(`Você realmente deseja desativar o produto "${produto.item.nome}"?`);
-    
+
         if (confirmRemove) {
             // Suponha que você tenha o ID do responsável disponível
             const responsavelString = sessionStorage.getItem("responsavel");
-            const responsavel = responsavelString ? JSON.parse(responsavelString) : null; 
+            const responsavel = responsavelString ? JSON.parse(responsavelString) : null;
             const idResponsavel = responsavel ? responsavel.id : null;
-    
+
             // Armazena o item associado ao produto antes da remoção do produto
             const itemId = produto.item.id;
-    
+
         }
     };
 
@@ -65,50 +88,51 @@ const ConfiguracoesProdutos = () => {
     actioProduto.yellow.icon = "fa-solid fa-pen"
     actioProduto.yellow.iconFillInvert = false
     actioProduto.yellow.text = "Editar"
-    actioProduto.yellow.action = ()=>{navigate("/editando-produto")}
+    actioProduto.yellow.action = () => { navigate("/editando-produto") }
 
     const githubPath = "https://raw.githubusercontent.com/Grupo-2-Sustentare/sustentare-web/main/src/assets/images/items/"
 
     return (
         <>
             <div className={styles.divTopBar}>
-            <TopBar  title={"configurações de produtos"} showBackArrow={true} backNavigationPath={"/configuracoes-de-estoque"}/>
+                <TopBar title={"configurações de produtos"} showBackArrow={true} backNavigationPath={"/configuracoes-de-estoque"} />
             </div>
             <div className={styles.divPrincipal}>
-            {/* <div className={styles.divFiltroEBusca}>
+                {/* <div className={styles.divFiltroEBusca}>
                     <IconInput/>
                     <StreachList showTitle={false}/>
             </div>
             <hr></hr> */}
                 {/* <Product name="Arroz" quantity="50 kilogramas"  showCheckbox={false} addressImg="https://raw.githubusercontent.com/Grupo-2-Sustentare/sustentare-web/main/src/assets/images/items/arroz.webp"/> */}
                 {produtos.map((produto) => (
-                        // <Product key={categoria.id} name={categoria.nome} showImageOrIcon={false} />
-                        <Product
-                            // key={produtos.id}
-                            name={produto.item.nome}
-                            quantity={produto.qtdProdutoTotal + " " + produto.item.unidade_medida.nome}
-                            showCheckbox={false} 
-                            // addressImg=
-                            buttonsConfig={{
-                                yellow: {
-                                    icon: "fa-solid fa-pen",
-                                    text: "Editar",
-                                    action: () => handleEdit(produto),
-                                },
-                                red: {
-                                    icon: "fa-solid fa-trash",
-                                    text: "Remover",
-                                    action: () => navigate("/tela-de-confirmacao", { state: { produto: produto } }),
-                                }
-                            }}
-                        />
-                    ))}
+                    // <Product key={categoria.id} name={categoria.nome} showImageOrIcon={false} />
+                    <Product
+                        // key={produtos.id}
+                        name={produto.item.nome}
+                        quantity={produto.qtdProdutoTotal + " " + produto.item.unidade_medida.nome}
+                        showCheckbox={false}
+                        addressImg={existImagem(produto.item.id)}
+                        buttonsConfig={{
+                            yellow: {
+                                icon: "fa-solid fa-pen",
+                                text: "Editar",
+                                action: () => handleEdit(produto),
+                            },
+                            red: {
+                                icon: "fa-solid fa-trash",
+                                text: "Remover",
+                                action: () => navigate("/tela-de-confirmacao", { state: { produto: produto } }),
+                            }
+                        }}
+                    />
+                ))}
             </div>
             <div className={styles.divBotao}>
-            <Button insideText="Cadastrar novo produto" onClick={handleSave}/>
+                <Button insideText="Cadastrar novo produto" onClick={handleSave} />
             </div>
         </>
     );
+    
 };
 
 export default ConfiguracoesProdutos;
