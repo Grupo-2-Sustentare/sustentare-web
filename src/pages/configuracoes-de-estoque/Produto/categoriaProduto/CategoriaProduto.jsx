bb84d3ae6fdf6d67839f8976c4bbcb1ef5f1b1b2
PereiraGus <gustavo.oito@hotmail.com>
2024-11-24 18:58:07 -0300

    97 - Pesquisa e ordenação na CategoriaProduto.

--- src/pages/configuracoes-de-estoque/Produto/categoriaProduto/CategoriaProduto.jsx
+++ src/pages/configuracoes-de-estoque/Produto/categoriaProduto/CategoriaProduto.jsx
@@ -5,66 +5,54 @@ import TopBar from "../../../../components/TopBar/TopBar.jsx";
 import style from "../categoriaProduto/categoriaProduto.module.css";
 import api from "../../../../api.js";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import {errorToast} from "../../../../components/Toast/Toast";
+import styles from "../../../historico-operacoes/historicoOperacoes.module.css";
+import {EnumObjetosBusca, OPCOES_ORDENACAO, ordenacaoComPesquisa} from "../../../../tools/ModuloBusca";
 
 export default function CategoriaProduto({ }) {
-   const navigate = useNavigate();
-   //   const githubPath = "https://raw.githubusercontent.com/Grupo-2-Sustentare/sustentare-web/main/src/assets/images/categorias/";
-
-   //   let p = JSON.parse(sessionStorage.getItem("productBeingCreated"))
-
-   //   const categoriaList = [
-   //      {"heading": "Ingredientes do self service"},
-   //      {"heading": "Bebidas"},
-   //      {"heading": "Doces para encomenda"},
-   //      {"heading": "Produtos de limpeza"},
-   //      {"heading": "Utensilios de cozinha"},
-   //      {"heading": "Descartáveis"},
-   //      {"heading": "Condimentos avulsos"},
-   //   ]
-
-   const [categorias, setCategorias] = useState([]); // Estado para armazenar as categorias carregadas da API
-
+    const navigate = useNavigate();
+    
+    const [categorias, setCategorias] = useState([])
+    const [categoriasVisiveis, setCategoriasVisiveis] = useState([])
+    const [queryPesquisa, setQueryPesquisa] = useState(null)
+    const [ordenacao, setOrdenacao] = useState(null)
+    
    useEffect(() => {
-      api.get("/categorias")
-         .then((response) => {
-            setCategorias(response.data); // Armazena os dados da API no estado
-         })
-         .catch((error) => {
-            console.error("Erro ao buscar categorias:", error); // Trata erros
+      api.get("/categorias").then((res) => setCategorias(res.data))
+         .catch((err) => {
+             errorToast("Erro ao buscar categorias. Contare o suporte.")
+            console.error("Erro ao buscar categorias:", err);
          });
    }, []);
 
    const selectListItem = (categoria) => {
-      // Armazena o item selecionado na sessionStorage
       sessionStorage.setItem('selectedCategoria', JSON.stringify(categoria));
-
       navigate(sessionStorage.paginaRequisicao);
    };
 
+    useEffect(() => {
+        setCategoriasVisiveis(ordenacaoComPesquisa(categorias, queryPesquisa, ordenacao, EnumObjetosBusca.CATEGORIA))
+    }, [categorias, queryPesquisa, ordenacao])
+
    return (
-      <>
-         <div className={style.topBar}>
-            <TopBar title="Categoria do produto" showBackArrow={true} backNavigationPath={sessionStorage.paginaRequisicao} />
-         </div>
-
-         <div className={style.conteudoInicioTela}>
-            <IconInput placeholder={"Pesquisar por nome"} />
-            <StrechList showTitle={false} hint="Ordenar por nome" />
-         </div>
-         <hr></hr>
-         <div className={style.conteudoTela}>
-            {categorias.map(categoria => {
-               return <ListItem
-                  key={categoria.id}
-                  // fullBorderRadius={true}
-                  heading={categoria.nome}
-                  // adressImg={categoria.adressImg}
-                  onClick={() => selectListItem(categoria)}
+       <>
+           <TopBar title="Categoria do produto" showBackArrow={true} backNavigationPath={sessionStorage.paginaRequisicao}/>
+           <div className={styles.barraDeBusca}>
+               <IconInput onChange={(v) => setQueryPesquisa(v.target.value)} placeholder={"Pesquisa por usuário"}/>
+               <StrechList
+                   showTitle={false} items={OPCOES_ORDENACAO.Categoria} hint={"Opções de ordenação"}
+                   onChange={(v) => setOrdenacao(v)}
                />
-            })}
-         </div>
+           </div><hr/>
+           <div className={style.conteudoTela}>
+               {categoriasVisiveis?.map(categoria => {
+                   return <ListItem
+                       key={categoria.id} heading={categoria.nome} onClick={() => selectListItem(categoria)}
+                   />
+               })}
+           </div>
 
-      </>
+       </>
    );
 }
