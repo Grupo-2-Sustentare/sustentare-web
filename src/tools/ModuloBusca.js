99ede85cac313f59295567b0e7022f904ed3d259
PereiraGus <gustavo.oito@hotmail.com>
2024-11-24 19:14:25 -0300

    97 - Unidades de medida no módulo de busca e ordenação.

--- src/tools/ModuloBusca.js
+++ src/tools/ModuloBusca.js
@@ -1,5 +1,5 @@
 const EnumObjetosBusca = {
-    PRODUTO: "Produto", LOG: "Log", CATEGORIA: "Categoria", USUARIO: "Usuario"
+    PRODUTO: "Produto", LOG: "Log", CATEGORIA: "Categoria", USUARIO: "Usuario", UNIDADE_DE_MEDIDA: "Unidade de medida"
 }
 
 const OPCOES_ORDENACAO = {
@@ -8,7 +8,8 @@ const OPCOES_ORDENACAO = {
     ],
     "Log": ["Ação - Crescente", "Ação - Decrescente", "Usuário - Crescente", "Usuário - Decrescente"],
     "Categoria": ["Alfabética - Crescente", "Alfabética - Decrescente"],
-    "Usuario": ["Alfabética - Crescente", "Alfabética - Decrescente"]
+    "Usuario": ["Alfabética - Crescente", "Alfabética - Decrescente"],
+    "Unidade de medida": ["Alfabética - Crescente", "Alfabética - Decrescente", "Tipo - Crescente", "Tipo - Decrescente"]
 }
 
 function filtrar(itens, query, tipoObjeto){
@@ -20,6 +21,8 @@ function filtrar(itens, query, tipoObjeto){
         case EnumObjetosBusca.LOG:
             return itens.filter(item => item.nomeUsuario.toUpperCase().indexOf(query.toUpperCase()) !== -1)
         case EnumObjetosBusca.CATEGORIA:
+            return itens.filter(item => item.nome.toUpperCase().indexOf(query.toUpperCase()) !== -1)
+        case EnumObjetosBusca.UNIDADE_DE_MEDIDA:
             return itens.filter(item => item.nome.toUpperCase().indexOf(query.toUpperCase()) !== -1)
         default: throw new Error(`Nenhum método de pesquisa para objetos da classe "${tipoObjeto}"`)
     }
@@ -55,6 +58,15 @@ function ordenar(itens, ordenacao, tipoObjeto){
             return [...itens].sort((a, b) => a.nome.localeCompare(b.nome))
         case "Usuario: Alfabética - Decrescente":
             return [...itens].sort((a, b) => b.nome.localeCompare(a.nome))
+
+        case "Unidade de medida: Alfabética - Crescente":
+            return [...itens].sort((a,b)=> a.nome.localeCompare(b.nome))
+        case "Unidade de medida: Alfabética - Decrescente":
+            return [...itens].sort((a,b)=> b.nome.localeCompare(a.nome))
+        case "Unidade de medida: Tipo - Crescente":
+            return [...itens].sort((a,b)=> a.categoria.localeCompare(b.categoria))
+        case "Unidade de medida: Tipo - Decrescente":
+            return [...itens].sort((a,b)=> b.categoria.localeCompare(a.categoria))
 
         default: throw new Error(`Nenhum método de ordenação "${ordenacao}" para objetos da classe "${tipoObjeto}"`)
     }
