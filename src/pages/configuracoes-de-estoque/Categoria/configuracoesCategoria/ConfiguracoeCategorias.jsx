ac9204caa7e2376d73c1ab97bf93c55637a6a307
PereiraGus <gustavo.oito@hotmail.com>
2024-11-24 18:03:19 -0300

    97 - Limpeza de código.

--- src/pages/configuracoes-de-estoque/Categoria/configuracoesCategoria/ConfiguracoeCategorias.jsx
+++ src/pages/configuracoes-de-estoque/Categoria/configuracoesCategoria/ConfiguracoeCategorias.jsx
@@ -7,72 +7,42 @@ import Button from "../../../../components/Button/Button";
 import TopBar from "../../../../components/TopBar/TopBar";
 import IconInput from "../../../../components/IconInput/IconInput";
 import Product from "../../../../components/ProductItem/Product";
-import { successToast } from "../../../../components/Toast/Toast";
+import {errorToast} from "../../../../components/Toast/Toast";
 
 const ConfiguracoesCategorias = () => {
     const navigate = useNavigate();
-    const [categorias, setCategorias] = useState([]); // Estado para armazenar as categorias carregadas da API
+    const [categorias, setCategorias] = useState([]);
+
+    // Do módulo de busca e ordenação.
+    const [categoriasVisiveis, setCategoriasVisiveis] = useState([])
+    const [queryPesquisa, setQueryPesquisa] = useState(null)
+    const [ordenacao, setOrdenacao] = useState(null)
 
     useEffect(() => {
-        api.get("/categorias")
-            .then((response) => {
-                setCategorias(response.data); // Armazena os dados da API no estado
-            })
-            .catch((error) => {
-                console.error("Erro ao buscar categorias:", error); // Trata erros
+        api.get("/categorias").then((res) => setCategorias(res.data))
+            .catch((err) => {
+                errorToast("Erro ao buscar categorias.")
+                console.log("Erro ao buscar categorias:", err);
             });
     }, []);
-
-    // const categoriaLista = [
-    //     "Sobremesa encomenda", "Bebidas",
-    //     "Self-service", "Descartaveis"
-    // ]
-
-    const handleSave = () => {
-        navigate("/criando-nova-categoria");
-    };
 
     // Função para salvar a categoria na sessionStorage e navegar para a página de edição
     const handleEdit = (categoria) => {
         sessionStorage.setItem("categoria_selecionada", JSON.stringify(categoria)); // Salva a categoria na sessionStorage
         navigate("/editando-categoria"); // Redireciona para a página de edição
     };
-    
-    const handleRemove = (categoria) => {
-        const confirmRemove = window.confirm(`Você realmente deseja desativar a categoria "${categoria.nome}"?`);
-
-        if (confirmRemove) {
-            
-            const responsavelString = sessionStorage.getItem("responsavel");
-            const responsavel = responsavelString ? JSON.parse(responsavelString) : null;
-            const idResponsavel = responsavel ? responsavel.id : null;
-
-
-        }
-    };
-
-    // var actionCategoria = DEFAULT_BUTTON_CONFIG
-    // actionCategoria.yellow.style = {}
-    // actionCategoria.yellow.icon = "fa-solid fa-pen"
-    // actionCategoria.yellow.iconFillInvert = false
-    // actionCategoria.yellow.text = "Editar"
-    // actioCategoria.yellow.action = ()=>{navigate("/editando-categoria")}
 
     return (
         <>
             <TopBar title={"configurações de categorias"} showBackArrow={true} backNavigationPath={"/configuracoes-de-estoque"} />
             <div className={styles.divPrincipal}>
-                {/* <div className={styles.divFiltroEBusca}>
+                <div className={styles.divFiltroEBusca}>
                     <IconInput />
                     <StreachList showTitle={false} titulo=" " />
                 </div>
-                <hr></hr> */}
+                <hr></hr>
                 <div className={styles.principal}>
-                    {/* {categoriaLista.map(categoria=>{
-                        return <Product name={categoria} showImageOrIcon={false}/>
-                      })} */}
-
-                    {categorias.map((categoria) => (
+                    {categorias?.map((categoria) => (
                         // <Product key={categoria.id} name={categoria.nome} showImageOrIcon={false} />
                         <Product
                             key={categoria.id}
@@ -95,7 +65,7 @@ const ConfiguracoesCategorias = () => {
                 </div>
             </div>
             <div className={styles.divBotao}>
-                <Button insideText="Cadastrar nova categoria" onClick={handleSave} />
+                <Button insideText="Cadastrar nova categoria" onClick={()=>navigate("/criando-nova-categoria")} />
             </div>
         </>
     );
