531d1bbd0b1eac241fc8168dbf9fab7e2a60df96
PereiraGus <gustavo.oito@hotmail.com>
2024-11-24 19:27:51 -0300

    97 - Uso do módulo de pesquisa e ordenação em UnidadeMedidaProduto.

--- src/pages/configuracoes-de-estoque/Produto/unidadeDeMedidaProduto/UnidadeMedidaProduto.jsx
+++ src/pages/configuracoes-de-estoque/Produto/unidadeDeMedidaProduto/UnidadeMedidaProduto.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import IconInput from "../../../../components/IconInput/IconInput.jsx";
 import ListItem from "../../../../components/ListItem/ListItem.jsx";
 import StrechList from "../../../../components/StrechList/StrechList.jsx";
@@ -6,77 +6,55 @@ import TopBar from "../../../../components/TopBar/TopBar.jsx";
 import style from "../categoriaProduto/categoriaProduto.module.css";
 import api from "../../../../api.js";
 import { useNavigate } from "react-router-dom";
+import {errorToast} from "../../../../components/Toast/Toast";
+import {UNIT_ICONS} from "../../../../tools/TiposUnidadeMedida";
+import styles from "../../../historico-operacoes/historicoOperacoes.module.css";
+import {EnumObjetosBusca, OPCOES_ORDENACAO, ordenacaoComPesquisa} from "../../../../tools/ModuloBusca";
 
 export default function ConfigurationMenu({ }) {
-   const navigate = useNavigate();
-
-   // const unidadeMedidaList = [
-   //    { "icon": "fa-solid fa-cube", "heading": "Mililitro", "subheading": "Tipo: Volume - Abreviação: ml" },
-   //    { "icon": "fa-solid fa-hippo", "heading": "Quilograma", "subheading": "Tipo: Massa - Abreviação: kg" },
-   //    { "icon": "fa-solid fa-circle", "heading": "Quantidade Genérica", "subheading": "Tipo: Genérico - Abreviação: un" },
-   //    { "icon": "fa-solid fa-cube", "heading": "Sacas", "subheading": "Tipo: Volume - Abreviação: ml" },
-   //    { "icon": "fa-solid fa-hippo", "heading": "Galão", "subheading": "Tipo: Volume - Abreviação: ml" },
-
-   // ]
-
-   const [unidades, setUnidades] = useState([]);
+    const navigate = useNavigate();
+    const [unidades, setUnidades] = useState([]);
+    const [unidadesVisiveis, setUnidadesVisiveis] = useState([])
+    const [queryPesquisa, setQueryPesquisa] = useState(null)
+    const [ordenacao, setOrdenacao] = useState(null)
 
     useEffect(() => {
-        // Faz a requisição GET para buscar as unidades de medida
-        const fetchUnidades = async () => {
-            try {
-                const response = await api.get('/unidades-medida'); // Atualize a URL de acordo com sua API
-                setUnidades(response.data);
-            } catch (error) {
+        api.get('/unidades-medida').then((res) => setUnidades(res.data))
+            .catch((error)=>{
+                errorToast("Erro ao buscar unidade de medida. Contate o suporte.")
                 console.error("Erro ao buscar unidades de medida:", error);
-            }
-        };
-        fetchUnidades();
+            })
     }, []);
 
-
    const selectListItem = (unidadeMedida) => {
-      // Armazena o item selecionado na sessionStorage
       sessionStorage.setItem('selectedUnidadeMedida', JSON.stringify(unidadeMedida));
-
       navigate(sessionStorage.paginaRequisicao);
    };
 
-   const getIconByCategory = (categoria) => {
-      if (categoria === "Volume" || categoria === "volume") {
-          return "fa-solid fa-cube"; // Ícone para Volume
-      } else if (categoria === "Massa") {
-          return "fa-solid fa-hippo"; // Ícone para Massa
-      } else if (categoria === "Unidade" || categoria === "unidade" || categoria === "Genérico" || categoria === "genérico") {
-          return "fa-solid fa-circle"; // Ícone para Quantidade
-      } else {
-          return "fa-solid fa-question"; // Ícone padrão para outros tipos
-      }
-  };
+    useEffect(() =>
+        setUnidadesVisiveis(ordenacaoComPesquisa(unidades, queryPesquisa, ordenacao, EnumObjetosBusca.UNIDADE_DE_MEDIDA)),
+        [unidades, queryPesquisa, ordenacao]
+    )
 
    return (
-      <>
-         <div className={style.topBar}>
-            <TopBar title="Unidade de Medida do produto" showBackArrow={true} backNavigationPath={sessionStorage.paginaRequisicao} />
-         </div>
-
-         <div className={style.conteudoInicioTela}>
-            <IconInput placeholder={"Pesquisar por nome"} />
-            <StrechList showTitle={false} hint="Ordenar por nome" />
-         </div>
-         <hr></hr>
-         <div className={style.conteudoTela}>
-            {unidades.map(unidadeMedida => {
-               return <ListItem
-                  // fullBorderRadius={true}
-                  icon={getIconByCategory(unidadeMedida.categoria)}
-                  heading={unidadeMedida.nome}
-                  subheading={unidadeMedida.categoria}
-                  onClick={() => selectListItem(unidadeMedida)}
+       <>
+           <TopBar title="Unidade de Medida do produto" showBackArrow={true} backNavigationPath={sessionStorage.paginaRequisicao}/>
+           <div className={styles.barraDeBusca}>
+               <IconInput onChange={(v) => setQueryPesquisa(v.target.value)} placeholder={"Pesquisa por usuário"}/>
+               <StrechList
+                   showTitle={false} items={OPCOES_ORDENACAO["Unidade de medida"]} hint={"Opções de ordenação"}
+                   onChange={(v) => setOrdenacao(v)}
                />
-            })}
-         </div>
+           </div><hr/>
+           <div className={style.conteudoTela}>
+               {unidadesVisiveis?.map(unidade => {
+                   return <ListItem
+                       heading={unidade.nome} subheading={unidade.categoria} onClick={() => selectListItem(unidade)}
+                       icon={UNIT_ICONS[unidade.categoria.toUpperCase()]}
+                   />
+               })}
+           </div>
 
-      </>
+       </>
    );
 }
