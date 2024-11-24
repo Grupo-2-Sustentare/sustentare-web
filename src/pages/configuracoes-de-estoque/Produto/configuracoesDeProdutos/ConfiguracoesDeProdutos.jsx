ea507dec8765ed50cecd1b4962c072a00508ba86
PereiraGus <gustavo.oito@hotmail.com>
2024-11-24 17:50:59 -0300

    97 - Limpeza de código em Configurações de Produtos.

--- src/pages/configuracoes-de-estoque/Produto/configuracoesDeProdutos/ConfiguracoesDeProdutos.jsx
+++ src/pages/configuracoes-de-estoque/Produto/configuracoesDeProdutos/ConfiguracoesDeProdutos.jsx
@@ -7,24 +7,12 @@ import TopBar from "../../../../components/TopBar/TopBar";
 import IconInput from "../../../../components/IconInput/IconInput";
 import api from "../../../../api";
 import Product, { DEFAULT_BUTTON_CONFIG } from "../../../../components/ProductItem/Product";
-import { successToast } from "../../../../components/Toast/Toast";
+import {errorToast, successToast} from "../../../../components/Toast/Toast";
 import axios from "axios";
 
 const ConfiguracoesProdutos = () => {
-    const [imagens, setImagens] = useState({});
-    sessionStorage.removeItem('paginaRequisicao');
-    sessionStorage.removeItem('selectedUnidadeMedida');
-    sessionStorage.removeItem('selectedCategoria');
-    sessionStorage.removeItem("nome");
-    sessionStorage.removeItem("diasVencimento");
-    sessionStorage.removeItem("perecivel");
-    sessionStorage.removeItem("produto_selecionado");
-    sessionStorage.removeItem("selectedUnidadeMedida");
-    sessionStorage.removeItem("selectedCategoria");
     const navigate = useNavigate();
     const [produtos, setProdutos] = useState([]);
-
-
   
     async function carregarImagemAwsS3 (idUsuario)  {
         return axios.get(`https://teste-sustentare.s3.us-east-1.amazonaws.com//itens/imagens/${idUsuario}`)
@@ -34,36 +22,23 @@ const ConfiguracoesProdutos = () => {
             .catch(() => {
                 return `https://placehold.co/400/F5FBEF/22333B?text=Produto`;
             });
-    };
-    
-
+    }
 
     useEffect(() => {
-        const carregarProdutos = async () => {
-            try {
-                const response = await api.get("/produtos");
-                const produtos = response.data;
-    
-                const produtosComImagens = await Promise.all(
-                    produtos.map(async (produto) => {
-                        const imageUrl = await carregarImagemAwsS3(produto.item.id);
-                        return { ...produto, imageUrl };
-                    })
-                );
-    
-                setProdutos(produtosComImagens);
-            } catch (error) {
-                console.error("Erro ao carregar produtos ou imagens:", error);
-            }
-        };
-    
-        carregarProdutos();
+        sessionStorage.clear();
+         api.get("/produtos").then(async (res) => {
+             const produtosComImagens = await Promise.all(
+                 res.data?.map(async (prod) => {
+                     const imageUrl = await carregarImagemAwsS3(prod.item.id);
+                     return {...prod, imageUrl};
+                 })
+             );
+             setProdutos(produtosComImagens);
+         }).catch((err) => {
+             errorToast("Erro ao carregar produtos.")
+             console.log(err)
+         });
     }, []);
-    
-
-    const handleSave = () => {
-        navigate("/criando-produto");
-    };
 
     // Função para salvar a categoria na sessionStorage e navegar para a página de edição
     const handleEdit = (produto) => {
@@ -71,28 +46,12 @@ const ConfiguracoesProdutos = () => {
         navigate("/editando-produto"); // Redireciona para a página de edição
     };
 
-    const handleRemove = (produto) => {
-
-        const confirmRemove = window.confirm(`Você realmente deseja desativar o produto "${produto.item.nome}"?`);
-
-        if (confirmRemove) {
-            // Suponha que você tenha o ID do responsável disponível
-            const responsavelString = sessionStorage.getItem("responsavel");
-            const responsavel = responsavelString ? JSON.parse(responsavelString) : null;
-            const idResponsavel = responsavel ? responsavel.id : null;
-
-            // Armazena o item associado ao produto antes da remoção do produto
-            const itemId = produto.item.id;
-
-        }
-    };
-
-    let actioProduto = DEFAULT_BUTTON_CONFIG
-    actioProduto.yellow.style = {}
-    actioProduto.yellow.icon = "fa-solid fa-pen"
-    actioProduto.yellow.iconFillInvert = false
-    actioProduto.yellow.text = "Editar"
-    actioProduto.yellow.action = () => { navigate("/editando-produto") }
+    let actionProduto = DEFAULT_BUTTON_CONFIG
+    actionProduto.yellow.style = {}
+    actionProduto.yellow.icon = "fa-solid fa-pen"
+    actionProduto.yellow.iconFillInvert = false
+    actionProduto.yellow.text = "Editar"
+    actionProduto.yellow.action = () => { navigate("/editando-produto") }
 
     return (
         <>
@@ -100,7 +59,7 @@ const ConfiguracoesProdutos = () => {
                 <TopBar title={"configurações de produtos"} showBackArrow={true} backNavigationPath={"/configuracoes-de-estoque"} />
             </div>
             <div className={styles.divPrincipal}>
-                {produtos.length === 0 ? <p>Carregando usuarios...</p> : <p></p>}
+                {produtos.length === 0 ? <p>Carregando...</p> : <p></p>}
                 {produtos.map( (produto) => {
                     return <Product
                         name={produto.item.nome}
@@ -124,7 +83,7 @@ const ConfiguracoesProdutos = () => {
                 )}
             </div>
             <div className={styles.divBotao}>
-                <Button insideText="Cadastrar novo produto" onClick={handleSave} />
+                <Button insideText="Cadastrar novo produto" onClick={()=>navigate("/criando-produto")} />
             </div>
         </>
     );
