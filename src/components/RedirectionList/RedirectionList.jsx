bcdf18652c43d2ea324ae031f75c12f446670d7a
PereiraGus <gustavo.oito@hotmail.com>
2024-11-30 12:31:28 -0300

    108 - Trocando <select> da RedirectionList por <div>

--- src/components/RedirectionList/RedirectionList.jsx
+++ src/components/RedirectionList/RedirectionList.jsx
@@ -1,23 +1,25 @@
 import { useState } from "react";
 import styles from "./RedirectionList.module.css";
 import { useNavigate } from 'react-router-dom';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default function RedirectionList({title=undefined, redirectUrl, hint="Selecione..."}) {
     const navigate = useNavigate();
 
     const handleClick = () => {
-        if (redirectUrl == undefined){
-            alert("Defina uma URL para redirecionar!")
+        if (redirectUrl === undefined){
+            throw new Error("A RedirecionList criada não tem uma URL de destino definida!")
         }
         navigate(redirectUrl);
     };
     
     return (
         <div className={styles.container}>
-            {(title != undefined) && (<label>{title}</label>)}
-            <select className={styles.button} onClick={handleClick}>
-                <option value="">{hint}</option>
-            </select>
+            {(title !== undefined) && (<label>{title}</label>)}
+            <span onClick={handleClick}>
+                {hint}
+                <FontAwesomeIcon icon={"chevron-down"}/>
+            </span>
         </div>
     );
 }
