de5ef76a4e38c993d13be5bee13d0a4d96debbef
Julia-Justino <stephany.justino@sptech.school>
2024-12-03 23:46:07 -0300

    ajuste no proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,20 +1,37 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const DEBUG = false
+// Defina como true para ambiente local, ou false para ambiente em nuvem
+const DEBUG = false;
 
-module.exports = function(app) {
+// Função que configura o proxy
+module.exports = function (app) {
+    // Verifica se a variável DEBUG está definida corretamente
+    const targetUrl = DEBUG ? 'http://localhost:9000' : 'http://10.0.3.149:9000/';
+
+    console.log(`Configuração de proxy ativa. Target: ${targetUrl}`);
+
     app.use(
-        '/proxy-java-api',
+        '/java-api',
         createProxyMiddleware({
-            //10.0.0.164
-            //'http://10.0.0.164:8080'
-            target: DEBUG ? 'http://localhost:9000' : 'http://10.0.3.149:9000/',
+            target: targetUrl,
             changeOrigin: true,
-            pathRewrite: { '^/proxy-java-api': '' },
+            onError: (err, req, res) => {
+                console.error('Erro no Proxy:', err.message);
+                res.writeHead(500, {
+                    'Content-Type': 'text/plain',
+                });
+                res.end('Algo deu errado no proxy.');
+            },
+            onProxyReq: (proxyReq, req, res) => {
+                console.log(`Proxying request - Path: ${req.path}, Method: ${req.method}`);
+            },
+            onProxyRes: (proxyRes, req, res) => {
+                console.log(`Response recebido do proxy - Status: ${proxyRes.statusCode}`);
+            },
         })
     );
 
-    // app.use(
+        // app.use(
     //     '/lambda-services',
     //     createProxyMiddleware({
     //         target: 'https://pk0cpzwo89.execute-api.us-east-1.amazonaws.com',
