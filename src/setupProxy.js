e0b652273c511a71588d47ac84f8ef11daf57127
Julia-Justino <stephany.justino@sptech.school>
2024-12-04 00:07:55 -0300

    teste

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,43 +1,41 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// Define como true para ambiente local ou false para ambiente em nuvem
+// Defina como true para ambiente local, ou false para ambiente em nuvem
 const DEBUG = false;
 
-// Função para configurar os proxies
+// Função que configura o proxy
 module.exports = function (app) {
-    // Configuração para a Java API
-    const javaApiTarget = DEBUG ? 'http://localhost:9000' : 'http://10.0.3.149:9000/';
-    console.log(`Configuração de proxy para Java API ativa. Target: ${javaApiTarget}`);
+    // Verifica se a variável DEBUG está definida corretamente
+    const targetUrl = DEBUG ? 'http://localhost:9000' : 'http://10.0.3.149:9000/';
+
+    console.log(`Configuração de proxy ativa. Target: ${targetUrl}`);
 
     app.use(
         '/java-api',
         createProxyMiddleware({
-            target: javaApiTarget,
+            target: targetUrl,
             changeOrigin: true,
-            pathRewrite: { '^/java-api': '' }, // Remove o prefixo da URL ao repassar para o backend
             onError: (err, req, res) => {
-                console.error('Erro no Proxy (Java API):', err.message);
+                console.error('Erro no Proxy:', err.message);
                 res.writeHead(500, {
                     'Content-Type': 'text/plain',
                 });
-                res.end('Erro ao processar a solicitação no proxy Java API');
+                res.end('Algo deu errado no proxy.');
             },
             onProxyReq: (proxyReq, req, res) => {
-                console.log(`[Java API] Requisição.: ${req.method} ${req.path}`);
+                console.log(`Proxying request - Path: ${req.path}, Method: ${req.method}`);
             },
             onProxyRes: (proxyRes, req, res) => {
-                console.log(`[Java API] Resposta do Proxy: ${proxyRes.statusCode}`);
+                console.log(`Response recebido do proxy - Status: ${proxyRes.statusCode}`);
             },
         })
     );
 
-    /*
-    // app.use(
+        // app.use(
     //     '/lambda-services',
     //     createProxyMiddleware({
     //         target: 'https://pk0cpzwo89.execute-api.us-east-1.amazonaws.com',
     //         changeOrigin: true,
     //     })
-    // )
-    */
+    // );
 };
